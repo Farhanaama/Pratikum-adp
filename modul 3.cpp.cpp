c70f67c98869608bf06f2f6f75d77ea66f80af81
Farhanaama <163152507+Farhanaama@users.noreply.github.com>
2024-03-24 23:21:21 +0700

    Update modul 3.cpp

--- modul 3.cpp.cpp
+++ modul 3.cpp.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 string nama, nim, shift;
 
-
-void fibonacci(int n, int fib[]) {
+int fibonacci(int n, int fib[]) {
     fib[0] = 1;
     fib[1] = 1;
     for (int i = 2; i < n; ++i) {
         fib[i] = fib[i-1] + fib[i-2];
     }
+    return fib[n-1];
 }
 
 int main (){
